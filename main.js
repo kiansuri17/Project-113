a1d6aa54768f13ad0d1ac9d7dd727203e028e714
kiansuri17 <164382130+kiansuri17@users.noreply.github.com>
2024-03-28 19:22:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+    function preload(){
+    img = loadImage('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hpshop.co.za%2Fhp-blog%2Fpost%2F5-best-coding-programs-for-aspiring-programmers&psig=AOvVaw02N5-BXxTR-2jYkIqixJdT&ust=1711719958937000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjU8vOLl4UDFQAAAAAdAAAAABAE') ;
+}
+
+function setup(){
+    createCanvas(400, 400)
+}
+
+function setup() {
+    createCanvas(100, 100);
+  
+    background(200);
+  
+    rect(30, 20, 55, 55);
+  
+    describe('A white square with a black outline on a gray canvas.');
+  }
